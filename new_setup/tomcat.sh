f3868e3a5079de937a547624aef766e554108566
diwakarvenkatasai <123432726+diwakarvenkatasai@users.noreply.github.com>
2025-03-08 10:19:42 +0530

    Create tomcat.sh

---
+++ new_setup/tomcat.sh
@@ -0,0 +1,6 @@
+#---------------------------------install tomcat------------------
+sudo wget url https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.83/bin/apache-tomcat-9.0.83.tar.gz
+sudo tar -xvzf apache-tomcat-9.0.83.tar.gz #untar
+cd apache-tomcat-9.0.83
+cd bin
+chmod +x startup.sh
